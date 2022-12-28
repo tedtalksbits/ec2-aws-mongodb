04d44140b9b8b983e4ec51c8633398a8d1feafb7
tedtalksbits <tedaneblake@gmail.com>
2022-12-27 20:52:52 -0500

    font icons

--- public/static/js/rememberMe.js
+++ public/static/js/rememberMe.js
@@ -4,23 +4,25 @@ const rememberMePreference = localStorage.getItem('rememberMe');
 const username = document.getElementById('username');
 const usernameRemebered = localStorage.getItem('username') || '';
 
-if (rememberMePreference === 'true') {
-    rememberMe.checked = true;
-    username.value = usernameRemebered;
-} else {
-    rememberMe.checked = false;
-    username.value = '';
-}
-rememberMe.addEventListener('change', () => {
-    if (rememberMe.checked) {
-        rememberMe.value = 'true';
-        localStorage.setItem('rememberMe', 'true');
+if (rememberMe && username) {
+    if (rememberMePreference === 'true') {
+        rememberMe.checked = true;
+        username.value = usernameRemebered;
     } else {
-        rememberMe.value = 'false';
-        localStorage.setItem('rememberMe', 'false');
+        rememberMe.checked = false;
+        username.value = '';
     }
-});
+    rememberMe.addEventListener('change', () => {
+        if (rememberMe.checked) {
+            rememberMe.value = 'true';
+            localStorage.setItem('rememberMe', 'true');
+        } else {
+            rememberMe.value = 'false';
+            localStorage.setItem('rememberMe', 'false');
+        }
+    });
 
-username.addEventListener('input', () => {
-    localStorage.setItem('username', username.value);
-});
+    username.addEventListener('input', () => {
+        localStorage.setItem('username', username.value);
+    });
+}
